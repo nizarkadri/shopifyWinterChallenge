52bd3630c0e2dd44d73d382f1bfbb82043a7f6bc
nizarkadri <nizarkadri8@gmail.com>
2021-09-20 17:56:56 -0400

    fixing port issue--added process.env

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8080||8000;
+const port = process.env.PORT||8080;
 const app = express();
 const ejs = require('ejs');
 const fetch = require("node-fetch")
