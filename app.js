bb86316cf2439d68e826767945ace7ad188bb137
nizarkadri <nizarkadri8@gmail.com>
2021-09-20 12:07:45 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const port = 8080;
+const app = express();
+const ejs = require('ejs');
+const fetch = require("node-fetch")
+bodyParser = require('body-parser')
+var list;
+var date;
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.post('/search', (req, res) => {
+
+    date = req.body.query;
+    console.log("date searched = " + date);
+    res.sendStatus(200,"SUCCESSFULL");
+})
+app.get('/', (req, res) => {
+    var APOD;
+    
+    var uri = "https://api.nasa.gov/planetary/apod?api_key=aCvWJ9iFukrGCr6bo9MjutRU4dW9laPQyhUbT5T4"
+    var options = {
+        method: 'GET',
+    };
+    fetch(uri, options)
+        .then(response => response.text())
+        .then(result => {
+            console.log(result);
+            APOD = JSON.parse(result);
+            if (date == null)
+            {
+                console.log("date is NULL\n\n")
+                var uri = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-09-07&api_key=aCvWJ9iFukrGCr6bo9MjutRU4dW9laPQyhUbT5T4"    
+            }
+            else {
+                console.log("date is "+date+"\n\n")
+                var uri = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date="+date+"&api_key=aCvWJ9iFukrGCr6bo9MjutRU4dW9laPQyhUbT5T4"
+            }
+            
+            fetch(uri, options)
+                .then(response => response.text())
+                .then(result => {
+                    console.log("second api successfull_____________________"+result);
+                    list = JSON.parse(result);
+                    // res.render('index.ejs', {marsRover: list ,posterdata:APOD});
+                     res.render('index.ejs', {posterdata: APOD,mars:list });
+                })
+                // .catch(error => console.log('error', error));
+
+
+           
+        })
+        .catch(error => console.log('error', error));
+   
+    
+})
+
+app.listen(port, () =>{
+    console.log("Listening at 8080");
+})
+
+
+// row = document.getElementById("mainRow")
+
+// for (i = 0; i < 3; i++)
+// {
+//     card = document.createElement('div')
+//     card.className="col-md-auto"
+//     p = document.createElement('p')
+//     p.innerHTML = "Hi I am auot"
+//     card.appendChild(p)
+//     row.appendChild(card)
+    
+//     }
---
+++ public/main.js
@@ -0,0 +1,74 @@
+LikedPhotos={};
+LikedPhotos.id=[];
+$(window).on('load',function(){
+    $(".loader").fadeOut(1000);
+    $(".content").fadeIn(1000);
+    
+    $("#popup").fadeOut(5000);
+    if(localStorage.getItem("LikedPhotos")!=null){
+        
+        Liked=JSON.parse(localStorage.getItem("LikedPhotos"));
+        LikedPhotos.id=Liked.id;
+       console.log("array---",Liked.id);
+       
+        for(let i=0;i<Liked["id"].length;i++){
+             const wait_until_element_appear = setInterval(() => {
+                //  console.log(Liked["id"][i],"***********",$("#",Liked["id"][i])[0])
+                 if(document.getElementById(Liked["id"][i])==null){
+                     Liked.id.splice(Liked.id.indexOf(Liked.id[i]),1);
+                 }
+            else if(document.getElementById(Liked["id"][i]).length !== 0) {
+
+                console.log(document.getElementById(Liked["id"][i]).classList);
+                document.getElementById(Liked["id"][i]).classList.add("liked");
+
+                clearInterval(wait_until_element_appear);
+                }
+            }, 1000);
+            
+            
+        }
+    }
+})
+
+
+function post_date() {
+    console.log(document.querySelector('#dateSelector').value)
+    var uri = "http://" + window.location.host+"/search";
+    console.log("-----"+uri)
+    var requestOptions = {
+    method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ 'query':document.querySelector('#dateSelector').value })
+    // redirect: 'follow'
+    };
+    fetch(uri, requestOptions).then(response => response.text())
+        .then(result => {
+            console.log(result);
+          
+        })
+        .catch(error => console.log('error', error));
+    
+    window.open(window.location.href,"__self");
+}
+
+function liked(element) {
+    console.log(element.id)
+    let isLiked=document.getElementById(element.id).classList.contains("liked")
+    if (isLiked==false) {
+        console.log(element.classList);
+        element.classList.add("liked");
+        LikedPhotos.id.push(element.id);
+    }
+    else{
+        element.classList.remove("liked");
+        
+        LikedPhotos.id.splice(LikedPhotos.id.indexOf(element.id),1);
+    }
+    
+    localStorage.setItem("LikedPhotos",JSON.stringify(LikedPhotos));
+}
+
+
